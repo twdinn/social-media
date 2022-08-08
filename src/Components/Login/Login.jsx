9969ef36a2b1e008e4d531ac9b01a747066c5060
Tyler Dinn <twdinn@gmail.com>
2022-08-08 18:45:03 -0230

    Aug 8 6:45pm

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Login.css";
+import enterPic from "./enter-pic.jpg";
 
 const Login = () => {
   return (
     <div className="login">
-      <div className="login_form">
-        <form className="login_form_inputs" action="">
+      <div className="login_left">
+        <form className="login_form" action="">
           <input type="text" placeholder="Name or Username" />
           <input type="password" placeholder="password" />
           <input type="submit" value="Login" />
@@ -13,8 +14,9 @@ const Login = () => {
 
         <button className="sign_up">Sign up</button>
       </div>
-      <div>
-        <h1>Social Media App</h1>
+      <div className="login_right">
+        {/* <h1>Social Media App</h1> */}
+        <img className="login_pic" src={enterPic} alt="" />
       </div>
     </div>
   );
