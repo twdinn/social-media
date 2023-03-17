984fba5c7a3da72f9623d9dd49e2b34a1a07986e
twdinn <twdinn@gmail.com>
2023-03-17 03:28:15 -0230

    Remove Link

--- src/Components/NavGreeting.jsx
+++ src/Components/NavGreeting.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 // Displays Name on the NavBar if the User Logs in
 // Displays Username if the User Signs Up
 const NavGreeting = ({ name, userName }) => {
   return (
     <nav className="nav_greeting">
-      <Link to="/profile">
-        {name ? <h1>{name}'s Feed</h1> : <h1>{userName}'s Profile</h1>}
-      </Link>
+      {name ? <h1>{name}'s Feed</h1> : <h1>{userName}'s Profile</h1>}
     </nav>
   );
 };
